5ff4b593ba86c30c86d3d224134409a684e947e4
Sayanneogi007 <131947463+Sayanneogi007@users.noreply.github.com>
2025-09-18 20:06:14 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext();
 const AppContextProvider = (props) => {
   const currencySymbol = "$";
   const backendUrl =
-    import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
+    import.meta.env.VITE_BACKEND_URL || "https://doctor-appointment-system-backend-ieou.onrender.com";
 
   const [doctors, setDoctors] = useState([]);
   const [token, setToken] = useState(
